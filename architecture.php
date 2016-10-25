5860b72324b0cdc1eeb35a0f0028abece693c227
Kait Schorr <kaitlynschorr@gmail.com>
2016-10-25 10:45:20 -0500

    Implemented Masonry Grid
    Made the architecture gallery a masonry grid using only css:
    http://w3bits.com/css-masonry/

--- architecture.php
+++ architecture.php
@@ -3,10 +3,9 @@
 
 <div class="container">
     <div id="gallery-page">
-    <div class="masonry hide-bullets">
+        <div class="masonry hide-bullets">
             <?php include("gallery-script.php") ?>
         </div>
-
     </div>
 </div>
 
--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
+<div>
 <footer>
     <p>&copy; Paul Tenner 2016</p>
 </footer>
 </div> 
-<!-- /container -->        
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 
