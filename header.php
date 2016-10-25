fef571445e3bf3d8e53ae2a2ca5e43d48d337b85
Kait Schorr <kaitlynschorr@gmail.com>
2016-10-25 08:32:41 -0500

    Added Lightbox Plugin
    Added the lightbox plugin by jbutz and linked to header and footer

--- header.php
+++ header.php
@@ -16,6 +16,9 @@
         }
     </style>
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/bootstrap-lightbox.min.css">
+
+
     <link rel="stylesheet" href="css/main.css">
     
     <!-- FONTS -->
