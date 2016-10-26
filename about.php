ed0d15e0db171449d0ab1f04ecc1e15249ce95ed
Kait Schorr <kaitlynschorr@gmail.com>
2016-10-26 10:31:08 -0500

    Updated About page

--- about.php
+++ about.php
@@ -3,26 +3,26 @@
 <?php include("header.php") ?>
 
 <div class="container" id="wrapper">
-  <!-- Example row of columns -->
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2">
-      <h2 class="text-center">About Paul Tenner</h2>
-      <div class="well" id="about-text">
-        <img class="img-responsive pull-left" id="artist-photo" src="http://placehold.it/300x300" alt="">
-        <p>I have called San Antonio TX home for the last twenty years.  I grew up in El Paso TX and I became enamored with the State of New Mexico during my youth.  I was attracted to New Mexico’s culture, architecture, and geography.  I fully understand the allure of New Mexico that prompted Georgia O’Keeffe to travel there and make it her home.  The overwhelming majority of my photographic prints feature landscapes and unique architectural structures in Texas and New Mexico.</p>  
+	<!-- Example row of columns -->
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<h2 class="text-center">About Paul Tenner</h2>
+			<div class="well" id="about-text">
+				<img class="img-responsive pull-left" id="artist-photo" src="http://placehold.it/300x300" alt="">
+				<p>I have called San Antonio TX home for the last twenty years.  I grew up in El Paso TX and I became enamored with the State of New Mexico during my youth.  I was attracted to New Mexico’s culture, architecture, and geography.  I fully understand the allure of New Mexico that prompted Georgia O’Keeffe to travel there and make it her home.  The overwhelming majority of my photographic prints feature landscapes and unique architectural structures in Texas and New Mexico.</p>  
 
-        <p>The first time I saw a photograph that was printed on aluminum was during a trip I made to Santa Fe, NM in 2011.  The print was a black and white image in which the grey portions of the image allowed the underlying aluminum to shine through.  The image took on a different appearance as the light struck it from different angles.  After returning to Texas, I started investigating the process of printing on aluminum, for both black and white and color images.  Color images on aluminum also produce remarkable photographic prints.  The colors are vivid and appear to spring forth from the aluminum panel.</p>
+				<p>The first time I saw a photograph that was printed on aluminum was during a trip I made to Santa Fe, NM in 2011.  The print was a black and white image in which the grey portions of the image allowed the underlying aluminum to shine through.  The image took on a different appearance as the light struck it from different angles.  After returning to Texas, I started investigating the process of printing on aluminum, for both black and white and color images.  Color images on aluminum also produce remarkable photographic prints.  The colors are vivid and appear to jump out from the aluminum panel.</p>
 
-        <p>The result of my exploration into using aluminum as a print medium is Alumages by Paul™.  I print exclusively on aluminum. </p>
+				<p>The result of my exploration into using aluminum as a print medium is Alumages by Paul™.  I print exclusively on aluminum. </p>
 
-        <p>My prints are made using a dye-sublimation printing process to reproduce images onto aluminum panels.  For a more technical explanation as to the process, the following link will take you to a description of the <a href="printing-process.php">Printing Process</a>.  In addition to the richness and beauty of the images, they are light weight, fade and scratch resistant, and they do not require framing.</p>
+				<p>My prints are made using a dye-sublimation printing process to reproduce images onto aluminum panels.  For a more technical explanation as to the process, the following link will take you to a description of the <a href="printing-process.php">Printing Process</a>.  In addition to the richness and beauty of the images, they are light weight, fade and scratch resistant, and they do not require framing.</p>
 
-        <p>I hope you enjoy your visit to my website.  If you have any questions or comments, please click on the <a href="contact.php">Contact</a> to get in touch with me.</p>
+				<p>I hope you enjoy your visit to my website. If you are interested in purchasing a metal print of one of my images, or should you have any comments or questions, just click <a href="contact.php">Contact</a> to get in touch with me. Alumages can be made in a variety of sizes, from as small as 8” x 10” all the way up to 48” x 96”. I’d be happy to review with you image sizes that are possible for each individual image.</p>
 
-        <p>Last, but not least, I extend a huge thank you to Patricia, my wife and life partner for nearly 40 years, who exercises supreme patience when I don’t keep up with her pace during our nature hikes and sightseeing trips.  When I have a camera in my hand, she knows that I am likely be trailing behind.</p>
-      </div>
-    </div>
-  </div>
+				<p>Last, but not least, I need to throw out a few kudos. Thanks to my daughter for her design of my logo, to my son’s wife for providing me an outlet for my work using her web development skills, and to my wife, who has been my life partner for nearly 40 years and who exercises supreme patience when I don’t keep up with her pace during our nature hikes and sightseeing trips. When I have a camera in my hand, she knows that I am likely to be trailing behind.</p>
+			</div>
+		</div>
+	</div>
 </div>
 
 <?php include("footer.php") ?>
