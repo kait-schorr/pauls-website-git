78af75c751685fbd6b7c77f03a4a9a5e2b2eba72
Kait Schorr <kaitlynschorr@gmail.com>
2016-10-28 12:08:07 -0500

    Added Example Pages for Different Styles

--- architecture-fullscreen-lightbox.php
+++ architecture-fullscreen-lightbox.php
@@ -15,7 +15,7 @@ $folder_name = "images/architecture"; ?>
 			tile_enable_textpanel:true,
 			tile_textpanel_title_text_align: "center",
 			tiles_space_between_cols: 10,
-			lightbox_type: "compact",
+			tiles_type:"nested",
 			lightbox_slider_image_border_color: "black",
 		});
 
--- architecture-justified.php
+++ architecture-justified.php
@@ -16,6 +16,7 @@ $folder_name = "images/architecture"; ?>
 			tile_textpanel_title_text_align: "center",
 			tiles_space_between_cols: 10,
 			lightbox_type: "compact",
+			tiles_type:"justified",
 			lightbox_slider_image_border_color: "black",
 		});
 
