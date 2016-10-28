4effb98ded5de4ac89783e71c5c96254278663ed
Kait Schorr <kaitlynschorr@gmail.com>
2016-10-28 11:28:22 -0500

    Unitegallery Working w/ test Image

--- architecture.php
+++ architecture.php
@@ -1,15 +1,71 @@
 <?php $page_title = "Architecture Photos"; 
-$folder_name = "images/architecture";
-?>
+$folder_name = "images/architecture"; ?>
 <?php include("header.php") ?>
 
 <div class="container" id="wrapper">
-    <div id="gallery-page">
-        <div class="masonry hide-bullets">
-            <?php include("gallery-script.php") ?>
-        </div>
-    </div>
+	<div id="gallery-page" style="display:none;">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<img alt="River and Bridge"
+		src="images/architecture-thumbs/_30A6742.jpg"
+		data-image="images/architecture/_30A6742.jpg"
+		data-description="River and Bridge"
+		style="display:none">
+		<!-- <?php include("gallery-script.php") ?> -->
+	</div>
 </div>
+<script type="text/javascript">
+
+		jQuery(document).ready(function(){
+
+			jQuery("#gallery-page").unitegallery();
+
+		});
+		
+	</script>
 
 <?php include("footer.php") ?>
 </body>
