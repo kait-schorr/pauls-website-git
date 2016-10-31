7349f1ce158a9be31d0e320fbbd46124faea27a1
Kait Schorr <kaitlynschorr@gmail.com>
2016-10-31 11:17:20 -0500

    Added home page image folder and moved images

--- index.php
+++ index.php
@@ -1,45 +1,34 @@
 <?php $page_title = "Alumages by Paul Tenner"; ?>
 <?php include("header.php"); ?>
 <div class="container" id="wrapper">
-  <!-- Carousel for displaying main pictures -->
-
-  <div id="carousel-photo" class="carousel slide carousel-fade" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#carousel-photo" data-slide-to="0" class="active"></li>
-      <li data-target="#carousel-photo" data-slide-to="1"></li>
-      <li data-target="#carousel-photo" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner" role="listbox">
-      <div class="item active">
-        <img class="carousel-img" src="images/test-large/_AT_8547-2.jpg" alt="">
-        <div class="carousel-caption">
-        </div>
-      </div>
-      <div class="item">
-        <img class="carousel-img" src="images/test-large/_AT_8653-2.jpg" alt="...">
-        <div class="carousel-caption">
-        </div>
-      </div>
-      <div class="item">
-        <img class="carousel-img" src="images/test-large/_AT_9599-2.jpg" alt="...">
-        <div class="carousel-caption">
-        </div>
-      </div>
-    </div>
-
-    <!-- Controls -->
-    <a class="left carousel-control" href="#carousel-photo" role="button" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#carousel-photo" role="button" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>          
+  <!-- Image Grid -->
+<div id="home-page" style="display:none;">
+    <?php include("gallery-script.php") ?>
   </div>
+</div>
+<script type="text/javascript">
+
+  jQuery(document).ready(function(){
+
+    jQuery("#home-page").unitegallery({
+      gallery_theme: "tilesgrid",
+      grid_space_between_cols:10,
+      grid_space_between_rows:10,
+      tile_enable_border:false,
+      tile_enable_shadow:false,
+      grid_padding:0,
+      tile_width:250,
+      tile_height:250,
+      tile_enable_textpanel:true,
+      tile_textpanel_title_text_align: "center",
+      theme_navigation_type:"arrows",
+      lightbox_type: "compact",
+    });
+
+  });
+
+</script>
+
 
   <!-- Gallery Grid -->
   <div class="container">
