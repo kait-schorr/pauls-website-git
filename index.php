1f93e813cc75952b9337f11f1d21a4f88810b8d7
Kait Schorr <kaitlynschorr@gmail.com>
2016-10-31 11:24:03 -0500

    Added Unite Gallery to Homepage

--- index.php
+++ index.php
@@ -1,33 +1,12 @@
-<?php $page_title = "Alumages by Paul Tenner"; ?>
+<?php $page_title = "Alumages by Paul Tenner"; 
+$folder_name = "images/index"; ?>
 <?php include("header.php"); ?>
 <div class="container" id="wrapper">
   <!-- Image Grid -->
-<div id="home-page" style="display:none;">
+  <div id="home-page" style="display:none;">
     <?php include("gallery-script.php") ?>
   </div>
-</div>
-<script type="text/javascript">
 
-  jQuery(document).ready(function(){
-
-    jQuery("#home-page").unitegallery({
-      gallery_theme: "tilesgrid",
-      grid_space_between_cols:10,
-      grid_space_between_rows:10,
-      tile_enable_border:false,
-      tile_enable_shadow:false,
-      grid_padding:0,
-      tile_width:250,
-      tile_height:250,
-      tile_enable_textpanel:true,
-      tile_textpanel_title_text_align: "center",
-      theme_navigation_type:"arrows",
-      lightbox_type: "compact",
-    });
-
-  });
-
-</script>
 
 
   <!-- Gallery Grid -->
@@ -74,7 +53,29 @@
     </div>
   </div>
 
-<?php include("footer.php") ?>
+  <script type="text/javascript">
+
+    jQuery(document).ready(function(){
+
+      jQuery("#home-page").unitegallery({
+        gallery_theme: "tilesgrid",
+        grid_space_between_cols:10,
+        grid_space_between_rows:10,
+        tile_enable_border:false,
+        tile_enable_shadow:false,
+        grid_padding:0,
+        tile_width:250,
+        tile_height:250,
+        tile_enable_textpanel:true,
+        tile_textpanel_title_text_align: "center",
+        theme_navigation_type:"arrows",
+        lightbox_type: "compact",
+      });
+
+    });
+
+  </script>
+  <?php include("footer.php") ?>
 
 </body>
 </html>
